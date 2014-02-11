d0e0d254ec198aa3cef00b0d551561b7aed28988
Henrik Steen <henrist@henrist.net>
2014-02-11 03:39:48 +0100

    Add functions to fetch and partly model users and groups.

--- src/HenriSt/OpenLdapAuth/LdapUser.php
+++ src/HenriSt/OpenLdapAuth/LdapUser.php
@@ -4,6 +4,96 @@ use Illuminate\Auth;
 use HenriSt\OpenLdapAuth\Helpers\Ldap;
 
 class LdapUser implements Auth\UserInterface {
+	protected static $ldap_s;
+
+	public static function init()
+	{
+		if (static::$ldap_s) return;
+
+		// set up LDAP
+		$config = app()->config['auth']['ldap'];
+		static::$ldap_s = new Ldap($config);
+	}
+
+	/**
+	 * Find user
+	 *
+	 * @return LdapUser
+	 */
+	public static function find($username)
+	{
+		static::init();
+
+		$user = static::$ldap_s->get_user_details($username);
+		if (!is_null($user))
+		{
+			return new static($user, static::$ldap_s);
+		}
+	}
+
+	/**
+	 * Get all users
+	 *
+	 * @return array
+	 */
+	public static function all($get_groups = false)
+	{
+		static::init();
+
+		$data = static::$ldap_s->get_users();
+		$users = array();
+		foreach ($data as $user)
+		{
+			$users[] = new static($user, static::$ldap_s);
+		}
+
+		// get groups?
+		if ($get_groups)
+		{
+			static::fetchGroups($users);
+		}
+
+		return $users;
+	}
+
+	/**
+	 * Load groups for collection of users
+	 *
+	 * @param array users
+	 * @return void
+	 */
+	public static function fetchGroups($users)
+	{
+		static::init();
+
+		$list = array();
+		foreach ($users as $user)
+		{
+			$list[] = sprintf('(%s=%s)', static::$ldap_s->config['group_fields']['members'], Ldap::escape_string($user->username));
+		}
+
+		$groups = static::$ldap_s->get_groups(true, sprintf('(|%s)', implode("", $list)));
+
+		// reset groups data and make map
+		$map = array();
+		foreach ($users as $user)
+		{
+			$user->initGroups();
+			$map[$user->username] = $user;
+		}
+
+		// append groups to users
+		foreach ($groups as $group)
+		{
+			foreach ($group['members'] as $member)
+			{
+				if (isset($map[$member]))
+				{
+					$map[$member]->appendGroup($group);
+				}
+			}
+		}
+	}
 
 	/**
 	 * LDAP-object
@@ -99,6 +189,22 @@ class LdapUser implements Auth\UserInterface {
 	}
 
 	/**
+	 * Initialize groups array
+	 */
+	protected function initGroups()
+	{
+		$this->groups = array();
+	}
+
+	/**
+	 * Assign group to user internally
+	 */
+	protected function appendGroup($group)
+	{
+		$this->groups[] = $group;
+	}
+
+	/**
 	 * Dynamically access the user's attributes.
 	 *
 	 * @param  string  $key
@@ -143,4 +249,41 @@ class LdapUser implements Auth\UserInterface {
 		unset($this->attributes[$key]);
 	}
 
+	/**
+	 * Convert to array
+	 *
+	 * @param array Fields to ignore
+	 * @return array
+	 */
+	public function toArray(array $except = array())
+	{
+		$d = $this->attributes;
+		foreach ($except as $e)
+			unset($d[$e]);
+
+		// groups?
+		if ($this->groups)
+		{
+			$groups = array();
+			foreach ($this->groups as $group)
+			{
+				$groups[] = $group['name'];
+			}
+
+			return array_merge($d, array("groups" => $groups));
+		}
+
+		return $d;
+	}
+
+	/**
+	 * Check for same user
+	 *
+	 * @param LdapUser
+	 * @return bool
+	 */
+	public function isSame(LdapUser $user)
+	{
+		return $this->username == $user->username;
+	}
 }
